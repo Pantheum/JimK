6e00926b8971558341933c05e9cde2b26fa2dc17
JimK <inof@jimk.io>
2020-12-12 23:29:31 -0700

    0059
    0059

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    addr = "xpub6BgDJTqLBYyrPkj2fd8BJdLNAy49RV4e28dMYeziwhr7ZPcZStXyDnzQrWB8Zupk8oe23SDif4qcXDs3uy37LfeXXNzRoDZAmnw8ZXqWen7";
+    
+    
+  window.alert(addr);
+}
