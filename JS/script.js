0ae0253530daf10073411946d2aff0acbffd60b5
JimK <inof@jimk.io>
2020-12-13 01:34:54 -0700

    0063
    0063

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,14 @@
 function myFunction() {
     addr = "xpub6BgDJTqLBYyrPkj2fd8BJdLNAy49RV4e28dMYeziwhr7ZPcZStXyDnzQrWB8Zupk8oe23SDif4qcXDs3uy37LfeXXNzRoDZAmnw8ZXqWen7";
-  window.alert(addr);
+    
+    var copyhelper = document.createElement("input");
+  copyhelper.className = 'copyhelper'
+  document.body.appendChild(copyhelper);
+  copyhelper.value = addr;
+  copyhelper.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyhelper);
+    
+    
+  window.alert("Thank you for your support, it is much appreciated! My Bitcoin address has been copied to your clipboard!");
 }
